562303a2e5b0117c2a660fe64610b0334c1e2e43
Beckett <133655553+severian42@users.noreply.github.com>
2024-10-23 15:40:36 -0500

    Update ingestion.py

--- ingestion.py
+++ ingestion.py
@@ -14,9 +14,8 @@ logger = logging.getLogger(__name__)
 load_dotenv()
 
 urls = [
-    "https://lilianweng.github.io/posts/2023-06-23-agent/",
-    "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
-    "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
+    "",
+
 ]
 
 vectorstore = None
